c37b18007c6f70809c8551f7e5b044d8434e2a92
Sebastian <styralski98@gmail.com>
2024-06-04 17:19:33 +0200

    prg pattern added

--- kernel/priceChecker/views.py
+++ kernel/priceChecker/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
-from .services import products_search_service
+from django.shortcuts import render, redirect
+from .services import products_search_service, shop_filter_service
 
 def index(request):
     if request.method == 'POST':
         product_name = request.POST.get('product_name')
         products = products_search_service(product_name)
-        return render(request, 'priceChecker/index.html', {'products': products})
+        request.session['products'] = products
+        return redirect('priceChecker:results')
     else:
         return render(request, 'priceChecker/index.html')
+
+def results(request):
+    products = request.session.get('products', [])
+    if request.method == 'POST':
+        shop_name = request.POST.get('data_type')
+        if shop_name:
+            products = shop_filter_service(shop_name)
+    return render(request, 'priceChecker/results.html', {'products': products})
+    
+    
