80ca8bdb04ae5f930a10a3671fd1b08a0f4316f3
Sebastian <styralski98@gmail.com>
2024-06-11 21:19:58 +0200

    celery configuration

--- kernel/kernel/settings.py
+++ kernel/kernel/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# CELERY
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+
 
 # Application definition
 
