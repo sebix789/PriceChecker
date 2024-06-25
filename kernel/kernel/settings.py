10fd91c2527c3b8f7227552630c1f04bc8af0716
Sebastian <styralski98@gmail.com>
2024-06-25 13:13:55 +0200

    encapsulate secrets

--- kernel/kernel/settings.py
+++ kernel/kernel/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-od5lq4a(rc--qy7u9yo6bq4eh(dy4gvc+jv+)4u7v9&%-&t5vg'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
